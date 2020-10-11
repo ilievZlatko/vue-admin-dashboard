6cc5e9e7d17eab85e29df71c60b529f7224c2ea2
ilievZlatko <iliev.zlatko@gmail.com>
2020-10-11 11:48:01 +0100

    create teams page

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,37 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex'
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+	window.localStorage.setItem('isDarkMode', true)
+}
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+	state: {
+		isDarkMode: window.localStorage.getItem('isDarkMode'),
+	},
+
+	getters: {
+		isDarkMode(state) {
+			return state.isDarkMode
+		},
+	},
+
+	mutations: {
+		toggleDarkMode(state) {
+			if (state.isDarkMode) {
+				state.isDarkMode = false
+				document.body.style.background = '#f0f3f5'
+				window.localStorage.setItem('isDarkMode', state.isDarkMode)
+			} else {
+				state.isDarkMode = true
+				document.body.style.background = '#212c4f'
+				window.localStorage.setItem('isDarkMode', state.isDarkMode)
+			}
+		},
+	},
+
+	actions: {
+		triggerDarkMode(context) {
+			context.commit('toggleDarkMode')
+		},
+	},
+})
--- src/views/SignInFlow/SingIn.vue
+++ src/views/SignInFlow/SingIn.vue
@@ -1,23 +1,54 @@
 <template>
-  <div class="container">
-    <div class="request">
-      Don't have a Design+Code HQ account?
-      <router-link to="/request">Request an account</router-link>
-    </div>
+  <div
+    class="container"
+    :class="{ 'light-background': !isDarkMode, 'dark-background': isDarkMode }"
+  >
+    <request-account />
+
     <div class="login">
-      <img src="@/assets/DCHQ.svg" alt="" />
-      <h4>Sign into Design+Code HQ</h4>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+      <img src="@/assets/DCHQ.svg" alt="" v-show="isDarkMode" />
+      <img src="@/assets/DCHQ-dark.svg" alt="" v-show="!isDarkMode" />
+      <h4 :class="{ 'light-text': isDarkMode, 'dark-text': !isDarkMode }">
+        Sign into Design+Code HQ
+      </h4>
+      <input
+        type="email"
+        placeholder="Email"
+        :class="{ 'light-field': isDarkMode, 'dark-field': !isDarkMode }"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        :class="{ 'light-field': isDarkMode, 'dark-field': !isDarkMode }"
+      />
       <button>Sign In</button>
-      <router-link to="/recover">Forgot your password?</router-link>
+      <router-link
+        to="/recover"
+        :class="{ 'light-link': isDarkMode, 'dark-link': !isDarkMode }"
+        >Forgot your password?</router-link
+      >
+      <theme-switch />
     </div>
   </div>
 </template>
 
 <script>
+import RequestAccount from "@/components/RequestAccount";
+import ThemeSwitch from "@/components/ThemeSwitch";
+
 export default {
-  name: "SignIn"
+  name: "SignIn",
+
+  components: {
+    RequestAccount,
+    ThemeSwitch
+  },
+
+  computed: {
+    isDarkMode() {
+      return this.$store.getters.isDarkMode;
+    }
+  }
 };
 </script>
 
@@ -31,63 +62,5 @@ export default {
 
 .login {
   max-width: 400px;
-}
-
-.request {
-  position: absolute;
-  top: 40px;
-  right: 40px;
-  color: rgba(255, 255, 255, 0.3);
-
-  a {
-    color: white;
-  }
-}
-
-h4 {
-  margin: 0;
-  line-height: 34px;
-  font-size: 24px;
-  text-align: center;
-  color: #fff;
-}
-
-input {
-  background: rgba(255, 255, 255, 0.2);
-  border: 1px solid rgba(255, 255, 255, 0.2);
-  box-sizing: border-box;
-  border-radius: 4px;
-  height: 60px;
-  width: 100%;
-  font-size: 20px;
-  color: #fff;
-  padding-left: 20px;
-  margin-top: 20px;
-
-  &::placeholder {
-    color: rgba(255, 255, 255, 0.3);
-  }
-}
-
-button {
-  background: #56ccf2;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
-  border-radius: 4px;
-  height: 60px;
-  width: 100%;
-  font-size: 20px;
-  color: white;
-  border: none;
-  margin-top: 20px;
-  margin-bottom: 40px;
-}
-
-a {
-  line-height: 25px;
-  font-size: 16px;
-  text-align: center;
-  color: rgba(255, 255, 255, 0.3);
-  text-decoration: none;
-  margin-bottom: 40px;
 }
 </style>
